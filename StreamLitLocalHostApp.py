3532405d9769a10b991375f0f0ffeec22b922126
Quickmoves11 <quickmoves64@gmail.com>
2025-06-21 02:59:52 +0530

    Update StreamLitLocalHostApp.py

--- StreamLitLocalHostApp.py
+++ StreamLitLocalHostApp.py
@@ -44,56 +44,6 @@ y = df_model['stroke']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
 
-# Logistic Regression Code 
-
-class LogisticRegression:
-    def __init__(self, learning_rate=0.01, n_iters=1000, class_weight=None):
-        self.learning_rate = learning_rate
-        self.n_iters = n_iters
-        self.class_weight = class_weight
-        self.weights = None
-        self.bias = None
-
-    def sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
-
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
-        
-        if self.class_weight == 'balanced':
-            # Calculate class weights inversely proportional to class frequencies
-            class_counts = np.bincount(y)
-            total_samples = len(y)
-            class_weights = total_samples / (len(np.unique(y)) * class_counts)
-            weights = class_weights[y]
-        else:
-            weights = np.ones(n_samples)
-
-        # Gradient Descent
-        for i in range(self.n_iters):
-            linear_pred = np.dot(X, self.weights) + self.bias
-            predictions = self.sigmoid(linear_pred)
-
-            dw = (1 / n_samples) * np.dot(X.T, (weights * (predictions - y)))
-            db = (1 / n_samples) * np.sum(weights * (predictions - y))
-
-            self.weights = self.weights - self.learning_rate * dw
-            self.bias = self.bias - self.learning_rate * db
-
-    def predict(self, X):
-        linear_pred = np.dot(X, self.weights) + self.bias
-        y_pred = self.sigmoid(linear_pred)
-        class_pred = [0 if y <= 0.5 else 1 for y in y_pred]
-        return np.array(class_pred)
-
-    def score(self, X, y):
-        y_pred = self.predict(X)
-        accuracy = np.sum(y == y_pred) / len(y)
-        return accuracy
-
-
 #For Linear Regression
 linear_reg = LinearRegression()
 linear_reg.fit(X_train, y_train)
